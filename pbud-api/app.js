669530d0cba9ca82df76dc2a22862562a37ac35b
Zachary Hancock <zhancock278@gmail.com>
2015-07-30 10:09:19 -0400

    API: Added route/query to post/get practice items

--- pbud-api/app.js
+++ pbud-api/app.js
@@ -6,11 +6,17 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-var practiceItemSchema = require('models/practiceItem');
+var practiceItemSchema = require('./models/practiceItem');
 
 var practiceItems = require('./routes/practiceItems');
 
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.set('port', process.env.PORT || 3000);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,12 +26,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.listen(app.get('port'), function() {
+    console.log('Server listening on port ' + app.get('port'));
+});
+
 /*
  * Database connection and Schemas
  */
 
-var Show = mongoose.model('PracticeItem', practiceItemSchema);
-mongoose.connect('localhost');
+var PracticeItem = mongoose.model('PracticeItem', practiceItemSchema);
+mongoose.connect('localhost/pbud');
 
 /*
  * API ROUTES
--- pbud-api/routes/practiceItems.js
+++ pbud-api/routes/practiceItems.js
@@ -1,14 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+var PracticeItem = require('../models/practiceItem');
+
 /* GET list of practice items meta data */
 router.get('/practiceItems', function(req, res, next) {
-    res.send('respond with an item list');
+    var query = PracticeItem.find();
+
+    // just list all
+    query.exec(function(err, item) {
+        if(err) return next(err);
+        res.send(item);
+    });
 });
 
 /* GET practice item by id */
 router.get('/practiceItems/:itemId', function(req, res, next) {
     res.send('respond with an item');
+});
+
+/* POST new practice item */
+router.post('/practiceItems', function (req, res, next) {
+
+    var item = req.body;
+
+    // just create an item, no validation yet
+    var practiceItem = new PracticeItem({
+        _id: 1,
+        title: item.title,
+        artist: item.artist,
+        progress: item.progress,
+        lastPlayed: null,
+        playCount: 0,
+        mediaId: item.mediaId,
+        tabData: item.tabData,
+        lyricData: item.lyricData
+    });
+
+    practiceItem.save(function(err) {
+        if(err) {
+            return next(err);
+        }
+
+        // echo back on success for now
+        res.send(200, practiceItem);
+    })
 });
 
 module.exports = router;
